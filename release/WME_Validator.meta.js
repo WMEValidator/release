32c14c3309be81a8ea32ae23f79f86ee9ff49caf
Andriy Berestovskyy <berestovskyy@gmail.com>
2019-08-13 00:47:08 +0200

    New release

--- release/WME_Validator.meta.js
+++ release/WME_Validator.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name                WME Validator
-// @version             2019.04.11
+// @version             2019.08.13
 // @description         This script validates a map area in Waze Map Editor, highlights issues and generates a very detailed report with wiki references and solutions
 // @match               https://beta.waze.com/*editor*
 // @match               https://www.waze.com/*editor*
@@ -14,4 +14,5 @@
 // @license             GPLv3
 // @contributor         justins83
 // @contributor         davidakachaos
+// @contributor         jangliss
 // ==/UserScript==
