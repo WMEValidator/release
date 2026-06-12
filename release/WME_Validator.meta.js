29a2361f803456953a220d462084c2e59f0d1b10
Andriy Berestovskyy <berestovskyy@gmail.com>
2026-06-12 23:01:21 +0200

    Release 2026.06.12

--- release/WME_Validator.meta.js
+++ release/WME_Validator.meta.js
@@ -1,15 +1,18 @@
 // ==UserScript==
 // @name                WME Validator
-// @version             2025.02.26
+// @version             2026.06.12
 // @description         This script validates a map area in Waze Map Editor, highlights issues and generates a very detailed report with wiki references and solutions
 // @match               https://beta.waze.com/*editor*
 // @match               https://www.waze.com/*editor*
 // @exclude             https://www.waze.com/*user/*editor/*
 // @exclude             https://www.waze.com/discuss/*
 // @grant               none
+// @require             https://cdn.jsdelivr.net/npm/@turf/turf@7/turf.min.js
+// @require             https://cdn.jsdelivr.net/npm/@turf/turf@7/turf.min.js
 // @icon                https://raw.githubusercontent.com/WMEValidator/release/master/img/WV-icon96.png
 // @namespace           a
-// @homepage            https://www.waze.com/forum/viewtopic.php?f=819&t=76488
+// @homepage            https://www.waze.com/discuss/t/script-wme-validator-v2025-02-26-places-beta/44877
+// @homepage            https://www.waze.com/discuss/t/script-wme-validator-v2025-02-26-places-beta/44877
 // @author              Andriy Berestovskyy <berestovskyy@gmail.com>
 // @copyright           2013-2018 Andriy Berestovskyy
 // @license             GPLv3
